2e5d85d253fd8088a356fb97a6f6abbff347744f
Hyemin Heo <heohm128@gmail.com>
2023-08-29 03:29:50 +0900

    [Build Up] 초기 프로젝트 세팅

---
+++ MAKS/MAKS/MAKSApp.swift
@@ -0,0 +1,17 @@
+//
+//  MAKSApp.swift
+//  MAKS
+//
+//  Created by sole on 2023/08/29.
+//
+
+import SwiftUI
+
+@main
+struct MAKSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
