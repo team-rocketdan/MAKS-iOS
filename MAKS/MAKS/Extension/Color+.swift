f6a2ed73831f94e58f46d565d24da5781bfbaebd
Hyemin Heo <heohm128@gmail.com>
2023-08-29 07:40:51 +0900

    #3 [Add] Color system 세팅

---
+++ MAKS/MAKS/Extension/Color+.swift
@@ -0,0 +1,35 @@
+//
+//  Color+.swift
+//  MAKS
+//
+//  Created by sole on 2023/08/29.
+//
+
+import SwiftUI
+
+extension Color {
+    
+    static let mkMainColor: Color = .init(hex: 0x410ED3)
+    static let mkSubColor: Color = .init(hex: 0x1E1F23)
+    
+    static let mkPointColor: Color = .init(hex: 0xFF6924)
+    
+    static let mkGray700: Color = .init(hex: 0x303236)
+    static let mkGray600: Color = .init(hex: 0x4B4D53)
+    static let mkGray500: Color = .init(hex: 0x6E7178)
+    static let mkGray400: Color = .init(hex: 0x979A9F)
+    static let mkGray300: Color = .init(hex: 0xBFC1C5)
+    static let mkGray200: Color = .init(hex: 0xE2E3E5)
+    static let mkGray100: Color = .init(hex: 0xEBEFF7)
+    
+    // hex 값으로 색을 초기화합니다.
+    init(hex: UInt, alpha: Double = 1) {
+        self.init(
+            .sRGB,
+            red: Double((hex >> 16) & 0xff) / 255,
+            green: Double((hex >> 08) & 0xff) / 255,
+            blue: Double((hex >> 00) & 0xff) / 255,
+            opacity: alpha
+        )
+    }
+}
