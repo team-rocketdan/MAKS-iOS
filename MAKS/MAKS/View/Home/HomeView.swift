8e490529f47d2a422863127514cdbf3f98b66eed
Hyemin Heo <heohm128@gmail.com>
2023-09-01 23:51:10 +0900

    #9 [Feat] HomeView 구현

---
+++ MAKS/MAKS/View/Home/HomeView.swift
@@ -0,0 +1,41 @@
+//
+//  HomeView.swift
+//  MAKS
+//
+//  Created by sole on 2023/08/30.
+//
+
+import SwiftUI
+import MapKit
+
+struct HomeView: View {
+    @State var text: String = ""
+    var body: some View {
+        VStack(alignment: .leading, spacing: 0) {
+            TitleBar()
+            
+            MKSearchBar(text: $text)
+                .padding(.vertical, 10)
+                .padding(.horizontal, 20)
+            
+            Map(mapRect: .constant(.world))
+            Text("지금 떠오르는 매장")
+                .font(.system(size: 20,
+                              weight: .bold))
+                .padding(.top, 20)
+                .padding(.leading, 20)
+            
+            ScrollView {
+                ForEach(0..<5) {_ in
+                    MarketRowView()
+                        .padding(.top, 16)
+                        .padding(.horizontal, 20)
+                }
+            }
+           
+            
+        }
+        .frame(width: UIScreen.screenWidth)
+        
+    }
+}
