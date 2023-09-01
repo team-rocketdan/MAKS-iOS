1c7e77f284cb2001fb50fa8dcdb276dd6a4916e6
Hyemin Heo <heohm128@gmail.com>
2023-09-02 02:07:14 +0900

    #10 [Feat] OrderListView 구현

---
+++ MAKS/MAKS/View/OrderList/OrderListView.swift
@@ -0,0 +1,31 @@
+//
+//  OrderListView.swift
+//  MAKS
+//
+//  Created by sole on 2023/09/01.
+//
+
+import SwiftUI
+
+struct OrderListView: View {
+    var body: some View {
+        VStack {
+            Text("주문내역")
+                .font(.system(size: 24,
+                              weight: .bold))
+                .padding(.vertical, 10)
+                .padding(.horizontal, 20)
+            
+            ScrollView {
+                VStack(spacing: 30) {
+                    ForEach(0..<10) { _ in
+                        OrderRowView()
+                            .padding(.horizontal, 20)
+                        
+                    }
+                }
+            }
+            .padding(.top, 20)
+        }
+    }
+}
