d794750c200330384313401979b086254b1012f3
Hyemin Heo <heohm128@gmail.com>
2023-09-14 15:51:32 +0900

    #22 [Add] Menu Model 추가

---
+++ MAKS/MAKS/Model/Menu.swift
@@ -0,0 +1,16 @@
+//
+//  Menu.swift
+//  MAKS
+//
+//  Created by sole on 2023/09/12.
+//
+
+import Foundation
+
+struct Menu {
+    let id: UUID
+    let marketID: String
+    let name: String
+    let price: Int
+    let description: String
+}
