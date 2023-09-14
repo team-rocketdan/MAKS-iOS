7e271d2cbb1cea1727ff7a6ca4f2cced0beced3b
Hyemin Heo <heohm128@gmail.com>
2023-09-14 15:52:27 +0900

    #22 [Add] User Model 추가

---
+++ MAKS/MAKS/Model/User.swift
@@ -0,0 +1,17 @@
+//
+//  User.swift
+//  MAKS
+//
+//  Created by sole on 2023/09/12.
+//
+
+import Foundation
+
+struct User {
+    let id: UUID
+    let name: String
+    let email: String
+    let loginCenter: String
+    var profileImage: String
+    let createdAt: Date
+}
