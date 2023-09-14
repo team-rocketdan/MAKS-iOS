7a7a2b39bbc11bfeb084a39728734b58b49f4f09
Hyemin Heo <heohm128@gmail.com>
2023-09-14 15:51:07 +0900

    #22 [Add] Market Model 추가

---
+++ MAKS/MAKS/Model/Market.swift
@@ -0,0 +1,16 @@
+//
+//  Market.swift
+//  MAKS
+//
+//  Created by sole on 2023/09/12.
+//
+
+import Foundation
+
+struct Market {
+    let id: UUID
+    let name: String
+    let coverImage: String?
+    let coordinate: [Double]
+    let status: String 
+}
