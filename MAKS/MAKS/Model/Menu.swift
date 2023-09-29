70d168b2b9d0264c31fcfdd707a1bded746be81b
Hyemin Heo <heohm128@gmail.com>
2023-09-30 03:21:57 +0900

    #37 [Add] Menu 모델 추가

--- MAKS/MAKS/Model/Menu.swift
+++ MAKS/MAKS/Model/Menu.swift
@@ -7,10 +7,23 @@
 
 import Foundation
 
-struct Menu {
+struct Menu: Codable {
     let id: UUID
-    let marketID: String
+    let market: MarketID
     let name: String
     let price: Int
     let description: String
+    let imageURL: String?
+    
+    static let defaultModel: Menu = .init(id: .init(),
+                                          market: .init(id: .init()),
+                                          name: "감자",
+                                          price: 12300,
+                                          description: "고소하고 맛있는 강원도 감자",
+                                          imageURL: nil)
+    
+    
+    struct MarketID: Codable {
+        let id: UUID
+    }
 }
