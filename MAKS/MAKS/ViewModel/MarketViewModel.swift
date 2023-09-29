cb016c2338fc31b1d2299c63e31a49299c33aae1
Hyemin Heo <heohm128@gmail.com>
2023-09-30 03:23:27 +0900

    #37 [Feat] fetchMarkets 메서드 구현

--- MAKS/MAKS/ViewModel/MarketViewModel.swift
+++ MAKS/MAKS/ViewModel/MarketViewModel.swift
@@ -8,7 +8,22 @@
 import SwiftUI
 
 class MarketViewModel: ObservableObject {
+    let url: String = Bundle.main.infoDictionary?["ServerURL"] as? String ?? ""
+    let apiManager = APIManager<Market>()
+    @Published var markets: [Market] = []
     
+    /// 주변 가게들을 모두 fetch 해오는 메서드입니다.
+    func fetchMarkets() async throws {
+        let markets = try await apiManager.fetch("\(url)/markets")
+        DispatchQueue.main.async {
+            self.markets = markets
+        }
+    }
+    
+    /// 검색한 market을 모두 검색해서 fetch하는 메서드입니다.
+    func searchMarkets() {
+        
+    }
 }
 
 
