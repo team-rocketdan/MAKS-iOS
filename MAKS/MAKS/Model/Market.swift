7d57c59408e237689ed7d14c2d227cc22b001c7e
Hyemin Heo <heohm128@gmail.com>
2023-09-30 03:21:47 +0900

    #37 [Add] Market 모델 추가

--- MAKS/MAKS/Model/Market.swift
+++ MAKS/MAKS/Model/Market.swift
@@ -7,10 +7,27 @@
 
 import Foundation
 
-struct Market {
+enum MarketStatus: String {
+    case open = "영업 중"
+    case close = "준비 중"
+}
+
+struct Market: Codable {
+    
+    
     let id: UUID
     let name: String
     let coverImage: String?
     let coordinate: [Double]
-    let status: String 
+    let status: String
+    let description: String
+//    var menus: [Menu]
+    
+    static let defaultModel: Market = .init(id: .init(),
+                                            name: "크로플Love",
+                                            coverImage: nil,
+                                            coordinate: [],
+                                            status: MarketStatus.open.rawValue,
+                                            description: "강남의 유명한 크로플, 크로와상 맛집"
+                                            )
 }
