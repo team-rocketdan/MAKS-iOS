96a92db6b74b93b1bc00a81b2b7c1ae9273cf723
Hyemin Heo <heohm128@gmail.com>
2023-09-30 03:32:35 +0900

    #38 [Style] SearchView와 navigation 연결

--- MAKS/MAKS/View/MainRouteView.swift
+++ MAKS/MAKS/View/MainRouteView.swift
@@ -9,46 +9,58 @@ import SwiftUI
 
 struct MainRouteView: View {
     @State var selectedIndex: Int = 0
+
+    @State var isPresentedSearchView: Bool = false
     
     var focusedTab: TabBarIndex {
         TabBarIndex(rawValue: selectedIndex) ?? .home
     }
     
     var body: some View {
-        ZStack {
-            switch focusedTab {
-            case .home:
-                HomeView()
-                    .padding(.bottom, 75)
-            case .search:
-                SearchView()
-                    .padding(.bottom, 75)
-            case .order:
-                OrderListView()
-                    .padding(.bottom, 75)
-            case .myPage:
-                MyPageView()
-                    .padding(.bottom, 75)
-            }
-            
-            
-            VStack(spacing: 0) {
-                Spacer()
+        NavigationStack {
+            ZStack {
+                    switch focusedTab {
+                    case .home, .search:
+                        HomeView()
+                            .padding(.bottom, 75)
+                    case .order:
+                        OrderListView()
+                            .padding(.bottom, 75)
+                    case .myPage:
+                        MyPageView()
+                            .padding(.bottom, 75)
+                    }
                 
-                HStack {
+                
+                VStack(spacing: 0) {
                     Spacer()
                     
-                    MKFloatingButton {
-                        print("floating Button")
+                    HStack {
+                        Spacer()
+                        
+                        MKFloatingButton {
+                            print("floating Button")
+                        }
                     }
+                    .padding(.bottom, 20)
+                    .padding(.trailing, 20)
+                    
+                    MKTabBar(selectedIndex: $selectedIndex)
+                        .ignoresSafeArea()
+                        .frame(maxHeight: 72)
                 }
-                .padding(.bottom, 20)
-                .padding(.trailing, 20)
-                
-                MKTabBar(selectedIndex: $selectedIndex)
-                    .ignoresSafeArea()
-                    .frame(maxHeight: 72)
             }
+            .onChange(of: selectedIndex) { newValue in
+                if newValue == TabBarIndex.search.rawValue {
+                    isPresentedSearchView = true
+                    selectedIndex = TabBarIndex.home.rawValue
+                }
+            }
+            .navigationDestination(isPresented: $isPresentedSearchView) {
+                SearchView()
+                    .padding(.bottom, 75)
         }
+        }
+        
     }
 }
