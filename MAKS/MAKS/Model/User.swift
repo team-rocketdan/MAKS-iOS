5398d92f13998e002c75883fbc74f2720b1dcc38
Hyemin Heo <heohm128@gmail.com>
2023-09-30 03:22:27 +0900

    #37 [Add] User 모델 추가

--- MAKS/MAKS/Model/User.swift
+++ MAKS/MAKS/Model/User.swift
@@ -7,11 +7,23 @@
 
 import Foundation
 
-struct User {
+enum LoginCenter: String {
+    case kakao = "kakao"
+    case apple = "apple"
+}
+
+struct User: Codable {
     let id: UUID
     let name: String
     let email: String
     let loginCenter: String
-    var profileImage: String
+    var profileImage: String?
     let createdAt: Date
+    
+    static let defaultModel: User = .init(id: .init(),
+                                          name: "김이화",
+                                          email: "eee@ewhain.net",
+                                          loginCenter: LoginCenter.kakao.rawValue,
+                                          profileImage: "",
+                                          createdAt: .now)
 }
