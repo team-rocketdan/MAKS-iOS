0ea283c99832ec5983982756b535c7531292ea0c
Hyemin Heo <heohm128@gmail.com>
2023-09-30 03:34:54 +0900

    #38 [Style] Label에서 Button으로 변경

--- MAKS/MAKS/View/Component/MKSearchChip.swift
+++ MAKS/MAKS/View/Component/MKSearchChip.swift
@@ -9,32 +9,47 @@ import SwiftUI
 
 struct MKSearchChip: View {
     let text: String
+    let action: () -> (Void)
     
     var body: some View {
-        HStack(spacing: 8) {
-            Text(text)
-                .font(.system(size: 16,
-                              weight: .light))
-                .foregroundColor(.mkGray600)
-        
-            Button {
-                deleteSearchChip()
-            } label: {
-                Image("close")
-                    .renderingMode(.template)
-                    .resizable()
-                    .frame(width: 18,
-                           height: 18)
+        Button {
+            action()
+        } label: {
+            HStack(spacing: 8) {
+                Text(text)
+                    .font(.system(size: 16,
+                                  weight: .light))
                     .foregroundColor(.mkGray600)
+            
+                Button {
+                    deleteSearchChip()
+                } label: {
+                    Image("close")
+                        .renderingMode(.template)
+                        .resizable()
+                        .frame(width: 18,
+                               height: 18)
+                        .foregroundColor(.mkGray600)
+                }
             }
+            .padding(.vertical, 4)
+            .padding(.horizontal, 10)
+            .background(Color.mkGray100)
+            .cornerRadius(12)
         }
-        .padding(.vertical, 4)
-        .padding(.horizontal, 10)
-        .background(Color.mkGray100)
-        .cornerRadius(12)
     }
+    
+    //MARK: - deleteSearchChip
     
     func deleteSearchChip() {
         // have to fill
+    }
+}
+
+//MARK: - Previews
+
+struct MKSearchChip_Previews: PreviewProvider {
+    static var previews: some View {
+        MKSearchChip(text: "크로플") {}
     }
 }
