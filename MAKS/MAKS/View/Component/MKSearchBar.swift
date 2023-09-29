fad312ff92224c99d4fc6837452d21e479bfc69a
Hyemin Heo <heohm128@gmail.com>
2023-09-30 03:14:23 +0900

    #24 [Feat] 버튼으로 사용할 search Label 구현

--- MAKS/MAKS/View/Component/MKSearchBar.swift
+++ MAKS/MAKS/View/Component/MKSearchBar.swift
@@ -44,9 +44,47 @@ struct MKSearchBar: View {
         .cornerRadius(10)
     }
     
+    //MARK: - label
+    
+    var label: some View {
+        HStack(spacing: 10) {
+            Image("search.fill")
+                .renderingMode(.template)
+                .resizable()
+                .frame(width: 20,
+                       height: 20)
+                .foregroundColor(Color.mkGray500)
+            
+            Text("주변 매장들을 검색해보세요.")
+                .font(.system(size: 14,
+                              weight: .light))
+                .foregroundColor(.mkGray600)
+                
+            Spacer()
+            
+        }
+        .padding(.vertical, 12)
+        .padding(.horizontal, 20)
+        .background(Color.mkGray100)
+        .cornerRadius(10)
+    }
+    
+    //MARK: - clear
+    
     /// x버튼 탭 시 실행되는 메서드입니다.
     /// text 내 모든 문자열을 삭제합니다.
     func clear() {
         text.removeAll()
+    }
+}
+
+struct MKSearchBar_Previews: PreviewProvider {
+    static var previews: some View {
+        VStack {
+            MKSearchBar(text: .constant(""))
+            
+            MKSearchBar(text: .constant("")).label
+        }
+        .padding(20)
     }
 }
