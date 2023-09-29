ebc92ff619c9f8ca7bd0cdc299522afc38482072
Hyemin Heo <heohm128@gmail.com>
2023-09-30 03:23:48 +0900

    #37 [Feat] fetchMenusInMarket 메서드 구현

--- MAKS/MAKS/ViewModel/MenuViewModel.swift
+++ MAKS/MAKS/ViewModel/MenuViewModel.swift
@@ -6,8 +6,24 @@
 //
 
 import SwiftUI
+import Alamofire
+import SwiftyJSON
 
 class MenuViewModel: ObservableObject {
+    let url: String = Bundle.main.infoDictionary?["ServerURL"] as? String ?? ""
+    let apiManager = APIManager<Menu>()
+    
+    @Published var menus: [Menu] = []
+    
+    /// marketID로 검색하는 query 메서드
+    func fetchMenusInMarket(marketID: String) async throws {
+        let menus = try await apiManager.fetch("\(url)/menus/\(marketID)")
+        DispatchQueue.main.async {
+            self.menus = menus
+        }
+    }
+    
+    
     
 }
 
