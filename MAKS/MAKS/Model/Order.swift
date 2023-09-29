11f84674b528edb7ab17ca9aa1ea8bad7f72a971
Hyemin Heo <heohm128@gmail.com>
2023-09-30 03:22:10 +0900

    #37 [Add] Order 모델 추가

--- MAKS/MAKS/Model/Order.swift
+++ MAKS/MAKS/Model/Order.swift
@@ -7,7 +7,12 @@
 
 import Foundation
 
-struct Order {
+enum OrderStatus: String {
+    case prepare = "준비 중"
+    case done = "준비 완료"
+}
+
+struct Order: Codable {
     let id: UUID
     let userID: String
     let menus: [Menu]
@@ -15,4 +20,12 @@ struct Order {
     let createdAt: Date
     let isTakeOut: Bool
     let status: String
+    
+    static let defaultModel: Order = .init(id: .init(),
+                                           userID: "2345",
+                                           menus: [],
+                                           totalPrice: 122000,
+                                           createdAt: .now,
+                                           isTakeOut: false,
+                                           status: OrderStatus.done.rawValue)
 }
