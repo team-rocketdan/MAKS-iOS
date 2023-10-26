1cb47f0f4f292e9f7f99edb1444325340b12c692
Hyemin Heo <heohm128@gmail.com>
2023-10-26 16:24:49 +0900

    [Feat] 서버 응답에 맞춰 모델 수정

--- MAKS/MAKS/Model/Order.swift
+++ MAKS/MAKS/Model/Order.swift
@@ -14,18 +14,22 @@ enum OrderStatus: String {
 
 struct Order: Codable {
     let id: UUID
-    let userID: String
-    let menus: [Menu]
+    let userID: UUID
+    let marketID: UUID
+    let menus: [String : Int]
     let totalPrice: Int
-    let createdAt: Date
+//    let createdAt: Date
     let isTakeOut: Bool
     let status: String
+    let number: Int
     
     static let defaultModel: Order = .init(id: .init(),
-                                           userID: "2345",
-                                           menus: [],
+                                           userID: UUID(uuidString: "2A13752F-92CC-4A31-9E11-4FAE61FCFC5D") ?? .init(),
+                                           marketID: .init(),
+                                           menus: [:],
                                            totalPrice: 122000,
-                                           createdAt: .now,
+//                                           createdAt: .init(),
                                            isTakeOut: false,
-                                           status: OrderStatus.done.rawValue)
+                                           status: OrderStatus.done.rawValue,
+                                           number: 101)
 }
