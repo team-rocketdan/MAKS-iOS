13d2effbc55bc9310be0e7bd13d2f1b3218648c4
Hyemin Heo <heohm128@gmail.com>
2023-10-26 16:22:23 +0900

    [Chore] 주석 추가

--- MAKS/MAKS/Extension/Color+.swift
+++ MAKS/MAKS/Extension/Color+.swift
@@ -9,10 +9,16 @@ import SwiftUI
 
 extension Color {
     
+    //MARK: - Brand Color
+    
     static let mkMainColor: Color = .init(hex: 0x1E1F23)
     static let mkSubColor: Color = .init(hex: 0x303236)
     
+    //MARK: - Point Color
+    
     static let mkPointColor: Color = .init(hex: 0xFF6924)
+    
+    //MARK: - Gray Color
     
     /// 숫자값이 커질수록 진한 색입니다.
     static let mkGray700: Color = .init(hex: 0x303236)
@@ -25,13 +31,19 @@ extension Color {
     
     static let mkEmailGray: Color = .init(hex: 0x636363)
     
+    //MARK: - Login Color
+    
     static let kakaoContainerColor: Color = .init(hex: 0xFEE500)
     static let kakaoSymbolColor: Color = .init(hex: 0x000000)
     static let kakaoLabelColor: Color = .init(hex: 0x000000,
                                               alpha: 0.85)
     
+    //MARK: - TabBar Color
+    
     static let tabBarShadowColor: Color = .init(hex: 0x000000,
                                                 alpha: 0.18)
+    
+    //MARK: - init(hex)
     
     // hex 값으로 색을 초기화합니다.
     init(hex: UInt, alpha: Double = 1) {
