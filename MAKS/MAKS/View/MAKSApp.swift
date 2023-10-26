66c4dbec07742647223d8c2c8bb1deb74777bfe1
Hyemin Heo <heohm128@gmail.com>
2023-10-26 16:38:15 +0900

    [Add] 추가된 ViewModel 적용

--- MAKS/MAKS/View/MAKSApp.swift
+++ MAKS/MAKS/View/MAKSApp.swift
@@ -7,13 +7,16 @@
 
 import SwiftUI
 import NMapsMap
+import FirebaseCore
+
 
 class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
         NMFAuthManager.shared().clientId = Bundle.main.infoDictionary?["NMFClientID"] as? String ?? "unknown"
-
+        FirebaseApp.configure()
+        
        return true
     }
 }
@@ -26,6 +29,12 @@ struct MAKSApp: App {
     @StateObject var userViewModel: UserViewModel = .init()
     @StateObject var marketViewModel: MarketViewModel = .init()
     @StateObject var menuViewModel: MenuViewModel = .init()
+    @StateObject var orderViewModel: OrderViewModel = .init()
+    @StateObject var textToSpeech: TextToSpeech = .init()
+    @StateObject var speechRecognizer: SpeechRecognizer = .init()
+    @StateObject var chatGPTViewModel: ChatGPTViewModel = .init()
+    @StateObject var alertToastViewModel: AlertToastViewModel = .init()
+    @StateObject var navigationViewModel: NavigationViewModel = .init()
     
     var body: some Scene {
         WindowGroup {
@@ -34,6 +43,12 @@ struct MAKSApp: App {
                 .environmentObject(userViewModel)
                 .environmentObject(marketViewModel)
                 .environmentObject(menuViewModel)
+                .environmentObject(orderViewModel)
+                .environmentObject(speechRecognizer)
+                .environmentObject(chatGPTViewModel)
+                .environmentObject(textToSpeech)
+                .environmentObject(alertToastViewModel)
+                .environmentObject(navigationViewModel)
         }
     }
 }
