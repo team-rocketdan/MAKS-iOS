2618e06a0170499d3f005b4726b143189eabff7d
Hyemin Heo <heohm128@gmail.com>
2023-12-08 17:46:43 +0900

    [Refactor] navigator 주입 및 기존 코드 삭제

--- MAKS/MAKS/View/Home/HomeView.swift
+++ MAKS/MAKS/View/Home/HomeView.swift
@@ -6,26 +6,32 @@
 //
 
 import SwiftUI
+import LinkNavigator
 
 struct HomeView: View {
+    let navigator: LinkNavigatorType
+    
     @EnvironmentObject var navigationViewModel: NavigationViewModel
     @EnvironmentObject var marketViewModel: MarketViewModel
     @EnvironmentObject var menuViewModel: MenuViewModel
     
     @State var text: String = ""
     
-    @State var isPresentedSearchView: Bool = false
-    @State var isPresentedDetailMarketView: Bool = false
-    @State var isPresentedCartView: Bool = false
+//    @State var isPresentedSearchView: Bool = false
+//    @State var isPresentedDetailMarketView: Bool = false
+//    @State var isPresentedCartView: Bool = false
     @State var selectedMarket: Market?
     
     var body: some View {
         VStack(alignment: .leading, spacing: 0) {
-            TitleBar(isPresentedCartView: $isPresentedCartView)
+            TitleBar(navigator: navigator)
 
             Button {
                 // navigate to searchView
                 navigationViewModel.isPresentedSearchView = true
+                navigator.next(paths: [RouteMatchPath.searchView.rawValue],
+                               items: [:],
+                               isAnimated: true)
             } label: {
                 MKSearchBar(text: $text).label
                     .padding(.vertical, 10)
@@ -44,15 +50,11 @@ struct HomeView: View {
                 ForEach(marketViewModel.markets, id: \.id) { market in
                     MarketRowView(market: market) {
                         selectedMarket = market
-                        Task {
-                            do {
-                                try await menuViewModel.fetchMenusInMarket(marketID: market.id.uuidString)
-                            } catch {
-                                print("\(error.localizedDescription)")
-                            }
-                        }
                         
-                        navigationViewModel.isPresentedMarketView = true
+                        navigator.next(paths: [RouteMatchPath.marketDetailView.rawValue],
+                                       items: ["marketID": market.id.uuidString],
+                                       isAnimated: true)
+//                        navigationViewModel.isPresentedMarketView = true
                     }
                     .padding(.top, 16)
                     .padding(.horizontal, 20)
@@ -60,17 +62,17 @@ struct HomeView: View {
             }
         }
         .frame(width: UIScreen.screenWidth)
-        .navigationDestination(isPresented: $navigationViewModel.isPresentedSearchView) {
-            SearchView()
-                .environmentObject(navigationViewModel)
-                .environmentObject(marketViewModel)
-                .environmentObject(menuViewModel)
-        }
-        .navigationDestination(isPresented: $navigationViewModel.isPresentedMarketView) {
-            MarketDetailView(market: selectedMarket ?? .defaultModel)
-                .environmentObject(navigationViewModel)
-                .environmentObject(marketViewModel)
-                .environmentObject(menuViewModel)
-        }
+//        .navigationDestination(isPresented: $navigationViewModel.isPresentedSearchView) {
+//            SearchView()
+//                .environmentObject(navigationViewModel)
+//                .environmentObject(marketViewModel)
+//                .environmentObject(menuViewModel)
+//        }
+//        .navigationDestination(isPresented: $navigationViewModel.isPresentedMarketView) {
+//            MarketDetailView(market: selectedMarket ?? .defaultModel)
+//                .environmentObject(navigationViewModel)
+//                .environmentObject(marketViewModel)
+//                .environmentObject(menuViewModel)
+//        }
     }
 }
