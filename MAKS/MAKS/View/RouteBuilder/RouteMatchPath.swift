e0897615ba0c028b9f5e8b01679698426f5d0976
Hyemin Heo <heohm128@gmail.com>
2023-12-08 17:39:39 +0900

    [Refactor] RouteMatchPath 추가

---
+++ MAKS/MAKS/View/RouteBuilder/RouteMatchPath.swift
@@ -0,0 +1,21 @@
+//
+//  RouteMatchPath.swift
+//  MAKS
+//
+//  Created by sole on 11/14/23.
+//
+
+import Foundation
+
+enum RouteMatchPath: String {
+    case mainRouterView = "main"
+    case loginView = "login"
+    case searchView = "search"
+    case orderHistoryView = "order"
+    case myPageView = "myPage"
+    case marketDetailView = "marketDetail"
+    case cartView = "cart"
+    case payView = "pay"
+    case orderComplete = "orderComplete"
+    case searchResultView = "searchResult"
+}
